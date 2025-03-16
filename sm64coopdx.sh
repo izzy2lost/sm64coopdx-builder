0508273d4575344d06c5f4d5f61838843cc046c2
izzy2lost <izzynochill@gmail.com>
2025-03-15 21:38:47 -0400

    Add files via upload

--- sm64coopdx.sh
+++ sm64coopdx.sh
@@ -3,44 +3,15 @@ RESTART_INSTRUCTIONS="Dropping to shell. To rebuild, swipe from the top of your 
 if ! ls /storage/emulated/0 >/dev/null 2>&1
 then
     yes | pkg install termux-am
-fi
-cat <<EOF
-____ ____ ____ ___ 
-|    |  | |  | |__]
-|___ |__| |__| |   
-___  _  _ _ _    ___  ____ ____
-|__] |  | | |    |  \ |___ |__/
-|__] |__| | |___ |__/ |___ |  \\
-EOF
-echo 'Autodetecting baserom.us.z64. This can take a long time.'
-if [ -f ~/baserom.us.z64 ]
-then
-    BASEROM_PATH=~/baserom.us.z64
-EOF
-    echo 'Your device storage needs at least 2 GB free space to continue!'
-    echo $RESTART_INSTRUCTIONS
-    exit 1
-fi
-if [ -z "${BASEROM_PATH}" ]
-then
-    cat <<EOF
-_  _ ____    ____ ____ _  _
-|\ | |  |    |__/ |  | |\/|
-| \| |__|    |  \ |__| |  |
-EOF
-    echo 'Go to https://github.com/sanni/cartreader to learn how to get baserom.us.z64'
-    echo $RESTART_INSTRUCTIONS
-    exit 2
-else
-    cp "${BASEROM_PATH}" ~/baserom.us.z64
+    cp /storage/emulated/0/baserom.us.z64 ~/baserom.us.z64
 fi
 apt-mark hold bash
 yes | pkg upgrade -y
 yes | pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which
 cd
-if [ -d "sm64ex-omm" ]
+if [ -d "sm64coopdx" ]
 then
-    cp "${BASEROM_PATH}" sm64coopdx/baserom.us.z64
+    cp  /storage/emulated/0/baserom.us.z64 sm64coopdx/baserom.us.z64
     cd sm64coopdx
     git reset --hard HEAD
     git pull origin android
@@ -48,7 +19,7 @@ then
     make distclean
 else
     git clone --recursive https://github.com/ManIsCat2/sm64coopdx.git
-    cp "${BASEROM_PATH}" sm64coopdx/baserom.us.z64
+    cp  /storage/emulated/0/baserom.us.z64 sm64coopdx/baserom.us.z64
     cd sm64coopdx
 fi
 make 2>&1 | tee build.log

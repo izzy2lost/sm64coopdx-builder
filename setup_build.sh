fbd2fdd628449e767640eb4c8f2ed8d37ab594be
izzy2lost <izzynochill@gmail.com>
2025-03-16 07:22:48 -0400

    Add files via upload

--- setup_build.sh
+++ setup_build.sh
@@ -1,7 +1,8 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Update Termux and install dependencies
-pkg update -y && pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which -y
+pkg update -y
+pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which -y
 
 # Clone the repository
 git clone --recursive https://github.com/ManIsCat2/sm64coopdx.git

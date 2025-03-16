d850ac86d40a69418151bbc2f877c1e8999541b8
izzy2lost <izzynochill@gmail.com>
2025-03-16 00:40:27 -0400

    Add files via upload

--- setup_build.sh
+++ setup_build.sh
@@ -1,15 +1,14 @@
 #!/data/data/com.termux/files/usr/bin/bash
 
 # Update Termux and install dependencies
-pkg update -y && pkg upgrade -y
-pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which -y
+pkg update -y && pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which -y
 
 # Clone the repository
-git clone --recursive https://github.com/ManIsCat2/sm64coopdx.git $HOME/sm64coopdx
+git clone --recursive https://github.com/ManIsCat2/sm64coopdx.git
 
 # Move baserom into the project directory
-mv $HOME/baserom.us.z64 $HOME/sm64coopdx/
+mv baserom.us.z64 sm64coopdx/baserom.us.z64
 
 # Build the project
-cd $HOME/sm64coopdx
+cd sm64coopdx
 make

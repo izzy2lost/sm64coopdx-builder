78333abf66d5ff3d0f3d5095971fbd1a152a601c
izzy2lost <izzynochill@gmail.com>
2025-03-15 21:55:02 -0400

    Add files via upload

--- sm64coopdx.sh
+++ sm64coopdx.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
-RESTART_INSTRUCTIONS="Dropping to shell. To rebuild, swipe from the top of your screen, touch the arrow on the right side of your Termux Notification, touch 'Exit', then relaunch this app."
-if ! ls /storage/emulated/0 >/dev/null 2>&1
-then
-    yes | pkg install termux-am
     cp /storage/emulated/0/baserom.us.z64 ~/baserom.us.z64
-fi
 pkg upgrade -y
 pkg install git wget make python getconf zip apksigner clang binutils libglvnd-dev aapt which -y
     git clone --recursive https://github.com/ManIsCat2/sm64coopdx.git
-    cp  /storage/emulated/0/baserom.us.z64 sm64coopdx/baserom.us.z64
+    cp /storage/emulated/0/baserom.us.z64 sm64coopdx/baserom.us.z64
     cd sm64coopdx
 fi
 make 2>&1 | tee build.log
